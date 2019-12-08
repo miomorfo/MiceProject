386f1b9ba1e3322517c0a0f87c9bc70a2f47de89
Fernando <fernando.eb@icloud.com>
2019-12-07 22:41:23 -0300

    se crea proyecto con lógicas de físicas de salto

---
+++ MiceWalkers/KinematicBody2D.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+#la clase Vector2 contiene el sistema de cordenadas para poder movernos por el escenario
+var movimiento = Vector2()
+const UP = Vector2(0, -1)
+const GRAVEDAD = 20
+const VELOCIDAD = 200
+const ALTURA_SALTO = -500
+
+func _physics_process(delta):
+	#gravedad
+	movimiento.y += GRAVEDAD
+	
+	#movimiento del personaje en el eje X
+	if Input.is_action_pressed("ui_right"):
+		movimiento.x = VELOCIDAD
+	elif Input.is_action_pressed("ui_left"):
+		movimiento.x = -VELOCIDAD
+	else:
+		#si no se presiona nada detener el personaje
+		movimiento.x = 0
+	
+	#se detecta el suelo
+	if is_on_floor():
+#		print("el personaje está tocanto el suelo")
+		if Input.is_action_just_pressed("ui_up"):
+			movimiento.y = ALTURA_SALTO
+		
+	movimiento = move_and_slide(movimiento, UP)
+	pass
